b514b7754b7717bc6e7589c565fc13e831bf814a
KENNYMOAH <kennyss12345@hotmail.com>
2017-08-10 22:41:18 +0800

    link font

--- index.ios.js
+++ index.ios.js
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
+    fontFamily: "PressStart2P",
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
