d644f4522b91200b7ae9a9e788cd9283ab8609fd
KENNYMOAH <kennyss12345@hotmail.com>
2017-08-10 22:51:29 +0800

    add orientation

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+import Orientation from 'react-native-orientation';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -28,6 +23,51 @@ export default class project_lol extends Component {
         </Text>
       </View>
     );
+  }
+
+  componentWillMount() {
+    // The getOrientation method is async. It happens sometimes that 
+    // you need the orientation at the moment the JS runtime starts running on device. 
+    // `getInitialOrientation` returns directly because its a constant set at the 
+    // beginning of the JS runtime. 
+ 
+    const initial = Orientation.getInitialOrientation();
+    if (initial === 'PORTRAIT') {
+      Orientation.lockToLandscape();
+    } else {
+      // do something else 
+    }
+  }
+ 
+  componentDidMount() {
+    // this locks the view to Portrait Mode 
+    Orientation.lockToLandscape();
+ 
+    // this locks the view to Landscape Mode 
+    // Orientation.lockToLandscape(); 
+ 
+    // this unlocks any previous locks to all Orientations 
+    // Orientation.unlockAllOrientations(); 
+ 
+    Orientation.addOrientationListener(this._orientationDidChange);
+  }
+ 
+  _orientationDidChange = (orientation) => {
+    if (orientation === 'LANDSCAPE') {
+      // do something with landscape layout 
+    } else {
+      // do something with portrait layout 
+    }
+  }
+ 
+  componentWillUnmount() {
+    Orientation.getOrientation((err, orientation) => {
+      console.log(`Current Device Orientation: ${orientation}`);
+    });
+ 
+ 
+    // Remember to remove listener 
+    Orientation.removeOrientationListener(this._orientationDidChange);
   }
 }
 
